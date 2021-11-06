b60e7339efc4a0bb8ebb58e985c57a628348bfab
PotatoesFall <markarkossa@gmail.com>
2021-11-06 16:07:55 +0100

    recalculate member debt on startup

--- domain/streepjes/streepjes.go
+++ domain/streepjes/streepjes.go
@@ -1,6 +1,9 @@
 package streepjes
 
 import (
+	"fmt"
+	"time"
+
 	"github.com/FallenTaters/streepjes-api/domain/members"
 	"github.com/FallenTaters/streepjes-api/domain/orders"
 )
@@ -11,4 +14,43 @@ func DeleteMember(id int) error {
 	}
 
 	return members.ForceDelete(id)
+}
+
+func RecalculateMemberDebt() error {
+	allMembers, err := members.GetAll()
+	if err != nil {
+		return err
+	}
+
+	month := time.Now().Month()
+
+	for _, m := range allMembers {
+		allOrders, err := orders.GetByMemberID(m.ID)
+		if err != nil {
+			return err
+		}
+
+		debt := 0
+		for _, o := range allOrders {
+			if o.OrderTime.Month() != month {
+				continue
+			}
+
+			if o.Status == orders.OrderStatusCancelled {
+				continue
+			}
+
+			debt += o.Price
+		}
+
+		m.Debt = debt
+		err = members.Put(m)
+		if err != nil {
+			return err
+		}
+
+		fmt.Printf("new debt for %s: %.2f\n", m.Name, float64(m.Debt)/100)
+	}
+
+	return nil
 }
